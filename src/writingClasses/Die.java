6dc373361c19b6de90797dd9fa0838314eb872f8
Olga Karpenko <okarpenko@usfca.edu>
2018-08-27 12:47:47 -0700

    Added classes Die and RollingDice.

--- src/writingClasses/Die.java
+++ src/writingClasses/Die.java
@@ -1,24 +1,40 @@
 package writingClasses;
-
+/** A class that models a rolling die */
 public class Die {
-    public final static int MAX = 6;
-    private int faceValue;
+    public static final int MAX_SIDES = 6; // number of sides, a constant
+    private int faceValue; // face value, an instance variable of the class
 
+    /** Default constructor */
     public Die() {
         faceValue = 1;
     }
 
-
-    public int roll() {
-        faceValue = (int)(Math.random() * MAX) + 1;
-        return faceValue;
+    /** Another constructor for class Die
+     * that takes a parameter
+     * @param faceValue initial value of faceValue
+     */
+    public Die(int faceValue) {
+        this.faceValue = faceValue;
     }
 
     public int getFaceValue() {
         return faceValue;
     }
 
-    public String toString() {
-        return "Face value:" + faceValue;
+    /**
+     * Rolls the die: by generating a random integer from 1 to 6
+     */
+    public void roll() {
+        faceValue = (int)(Math.random() * MAX_SIDES) + 1;
     }
+
+    /**
+     * Returns the string that contains face value
+     * @return face value as a string
+     */
+    public String toString() {
+        String res =  "Face value: " + faceValue;
+        return res;
+    }
+
 }
--- src/writingClasses/RollingDice.java
+++ src/writingClasses/RollingDice.java
@@ -1,12 +1,16 @@
 package writingClasses;
-
+/** A class that demonstrates how to use class Die.
+ * From the book by Lewis&Loftus, Java Software solutions.
+ * */
 public class RollingDice {
     public static void main(String[] args) {
-        Die die1 = new Die();
-        Die die2 = new Die();
+        // Create two dice and roll them
+        Die die1  = new Die(5); // constructor with an argument with be invoked
+        Die die2  = new Die(); //default constructor will be invoked
         die1.roll();
         die2.roll();
-        System.out.println(die1.getFaceValue());
-        System.out.println(die2.getFaceValue());
+        System.out.println(die1); // Java calls toString method from class Die
+        System.out.println(die2); // Java calls toString method from class Die
+
     }
 }
